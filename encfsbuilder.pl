a4203cf4d13e2bb5f3c5f214c849e379042ca42d
rapier <rapier@psc.edu>
2016-03-18 16:06:56 -0400

    Add comments and TODOs

--- encfsbuilder.pl
+++ encfsbuilder.pl
@@ -55,14 +55,14 @@ my $cfg_path = "/usr/local/etc/encfsbuilder.cfg";
 my %options = ();
 my $DBH;
 
+# read the configuration file and put everything into
+# the global config data structure
 sub readConfig {
     if (! -e $cfg_path) {
 	print "Config file not found at $cfg_path. Exiting.\n";
 	exit;
     } else {
 	$config = Config::Tiny->read($cfg_path);
-	#use Data::Dump;
-	#dd $config;
 	my $error = $config->errstr();
 	if ($error ne "") {
 	    print "Error: $error. Exiting.\n";
@@ -112,7 +112,7 @@ sub validateConfig {
     if (! defined $config->{paths}->{master_chroot}) {
 	print "Missing path to master chroot directory. Exiting\n";
 	exit;
-    } else {
+    } else { #make sure it exists
 	if (! -e $config->{paths}->{master_chroot}) {
 	    print "Master chroot directory not found at $config->{paths}->{master_chroot}\n";
 	    exit;
@@ -121,7 +121,7 @@ sub validateConfig {
     if (! defined $config->{paths}->{encfs}) {
 	print "Missing path to encfs binaries in config file. Exiting.\n";
 	exit;
-    } else {
+    } else { # make sure it's there
 	if (! -e $config->{paths}->{encfs}) {
 	    print "Absolute path to encfs binary is incorrect. Exiting.\n";
 	    exit;
@@ -130,7 +130,7 @@ sub validateConfig {
     if (! defined $config->{paths}->{fusermount}) {
 	print "Missing path to fusermount binary in config file. Exiting.\n";
 	exit;
-    } else {
+    } else { # make sure we can find it
 	if (! -e $config->{paths}->{fusermount}) {
 	    print "Absolute path to fusermount binary is incorrect. Exiting.\n";
 	    exit;
@@ -138,6 +138,8 @@ sub validateConfig {
     }
 }
 
+# create a new pem key set
+# and return them as strings
 sub generateKeys {
     my $pk = Crypt::PK::DSA->new();
     $pk->generate_key(40,512);
@@ -147,36 +149,54 @@ sub generateKeys {
     return ($public_pem, $private_pem);
 }
 
+#generate a random password/string of text/whatever
+# that is 25 characters long
 sub generatePassword {
+    # instantiate a new random number generator
     my $rng = Bytes::Random::Secure::Tiny->new;
     my $validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    
+ 
+    #randomly picks 25 chacaters from the validChars string
     my $string = $rng->string_from($validChars, 25);
     
     print "Generated random string\n";
     return $string;
 }
 
+#encrypt our known text with our public key
 sub encryptKnownText {
     my $pubkey = shift @_;
     my $text = shift @_;
+    # instantiate a new PK object
     my $enc = Crypt::PK::DSA->new();
+    # import our public key into it
     $enc->import_key(\$pubkey);
+    # encrypt out text with sha256
     $text = $enc->encrypt($text, 'SHA256');
     return($text);
 }
 
+#decrypt our cipher string with the private key
+#not really used in this coe was included for error checking
 sub decryptKnownText {
     my $privkey = shift @_;
     my $text = shift @_;
+    #instantiate new PK object
     my $dec = Crypt::PK::DSA->new();
+    #import the private key
     $dec->import_key(\$privkey);
+    # decrypt the cipher with SHA256
     $text = $dec->decrypt($text, 'SHA256');
     return($text);
 }
 
+#generate a new UUID
+#UUIDs of this type are not gauranteed to be unique
+#but the likelihood of their being a collision is incredibly small
 sub generateUUID {
+    #instantiate the UUID object
     my $uuid_obj = Data::UUID->new;
+    #generate a UUID string
     my $uuid = $uuid_obj->create_str();
     print "Generated UUID\n";
     return ($uuid);
@@ -196,10 +216,12 @@ sub mountENCFS {
     print "Public directory: $pub_dir\n";
     # make the directories in advance so encfs doesn't ask for input
     # from the keyboard. Be sure to clear any existing directories. 
+    # if the directory exists remove everything in it
     if (-e $enc_dir) {
 	remove_tree ($enc_dir, {keep_root => 1});
     }
     if (! -w $enc_dir) {
+	#it doesn't exist or is not writeable. same thing. So try to make it.
 	if (! make_path($enc_dir)) {
 	    print "Failed to create encrypted encfs directory in target chroot. $! Exiting.\n";
 	    exit;
@@ -215,15 +237,18 @@ sub mountENCFS {
 	}
     }	
 
+    #This allows us to create an encfs mount without userinteraction
     system ("$encfs --standard --extpass=\"echo \'$password\'\" $enc_dir $pub_dir");
 
-    print "Mounted ENCFS\n";
     if ($? == -1) {
-	return $?;
-    }    
+	return -1;
+    }
+      
+    print "Mounted ENCFS\n";
     return 1;
 }
 
+# copy the testrig operational files to the encyrpted directory
 sub copyFiles {
     my $source = $config->{paths}->{source};
     my $destination = $options{p} . "/" . $config->{paths}->{pub_destination};
@@ -231,17 +256,20 @@ sub copyFiles {
     return(dircopy($source, $destination));
 }
 
+# unmount the encfs directory
 sub unmountENCFS {
     my $pub_dir = $options{p} . "/" . $config->{paths}->{pub_destination};
     my $fuser = $config->{paths}->{fusermount};
     system ("$fuser -q -u $pub_dir");
     if ($? == -1) {
-	return $?;
+	return -1;
     }
     print "ENCFS unmounted\n";
-   return 1;
+    return 1;
 }
 
+#there are individual configuration files unique
+# to each iso that have to be written to the Chroot target
 sub writeConfigToChroot {
     my $uuid = shift @_;
     my $known_enc = shift @_;
@@ -271,14 +299,23 @@ sub writeConfigToChroot {
 	return -1;
     }
     return 1;
+    # still need to hgave it write the NOC public key
+    # NOC host, NOC user, test configuration and so forth
 }
 
+#copy the master chroot to the target chroot
 sub copyMaster {
     my $masterpath = $config->{paths}->{master_chroot};
     print "Copying master chroot to target\n";
     return(dircopy($masterpath, $options{p}));
 }
 
+#TODO: Need to get NOC configuaration options from the
+# database and write them to the client table. This would include
+# values for the maximum number of runs, valid to dates, and
+# the public key to write the data back to the NOCs server
+
+#write the results to the database
 sub writeToDB {
     my $uuid = shift @_;
     my $public_key = shift @_;
@@ -333,9 +370,6 @@ if (defined $options{f}) {
 	exit;
     }
 }
-
-
-
 
 #if the test directory doesn't exist create it
 # but if it does make sure it's entirely empty
