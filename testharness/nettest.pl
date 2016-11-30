778d9b8ed3c6f258551728359a12a60eb84c5c1b
Chris Rapier <rapier@psc.edu>
2016-11-30 10:01:50 -0500

    work on nettest

--- testharness/nettest.pl
+++ testharness/nettest.pl
@@ -13,6 +13,7 @@ use Try::Tiny; #try and catch
 use Capture::Tiny; #capture output from commands
 use IO::Socket;
 use File::Find;
+use Switch;
 
 # settings and defaults
 my $cfg_path = "/usr/local/etc/testrig/tr2.cfg";
@@ -43,10 +44,11 @@ sub logger {
 # look for it on the iso itself
 sub fetchUUID {
     # $UUID_path is a global
+    my $FH;
     if (! -e $UUID_path) {
 	logger ("crit", "Missing UUID file on this ISO");
     }
-    open (my $FH, "<",  $UUID_path);
+    open ($FH, "<",  $UUID_path);
     # should only be one line
     my $uuid = <$FH>;
     close ($FH);
@@ -145,6 +147,19 @@ sub runSystem {
     }   
 }
 
+sub storeOutput {
+    my $data = shift @_;
+    my $filename = shift @_;
+    my $path = "/tmp/results";
+    my $uuid = $config->{user}->{uuid};
+    my $filepath = $path . "/" . $uuid . "-" . $filename;
+    my $FH;
+    
+    open ($FH, ">", $filepath);
+    print $FH $data;
+    close $FH;
+}
+
 #------------Config Functions-----------#
 
 # read the configuration file and put everything into
@@ -188,14 +203,15 @@ sub validateConfig {
 }
 
 # authenticate with trmanager
-sub trmanager_auth {
+sub trmanagerAuth {
     # we need to open a connection to the trmanager
     # and pass them a specific string (AUTH:UUID:AUTHCODE)
     # the return value will be the passcode for the encrypted subdirectory
     # this qualifies as being autheticated and we can move on with the
     # process
+    use Switch;
     
-    $challenge_path = "/usr/local/etc/testrig/challenge";
+    my $challenge_path = "/usr/local/etc/testrig/challenge";
     # first get the challenge data that we will present to the server
     if (! -e $challenge_path) {
 	logger ("crit", "Local authentication data file missing");
@@ -211,7 +227,8 @@ sub trmanager_auth {
     my $sock = new IO::Socket::INET (
 	PeerAddr => 'testrig.psc.edu',
 	PeerPort => '20203',
-	Proto => 'tcp');
+	Proto => 'tcp',
+	Timeout => '5');
     if (! $sock) {
 	logger ("crit", "Could not make connection to authentication server.");
     }
@@ -219,7 +236,209 @@ sub trmanager_auth {
 
     # need some sort of timeout function here
     my $response = <$sock>;
-    
+    switch($response) {
+	case "NODBH" {
+	    logger ("crit", "The authentication manager could not connect to the database.");
+	} 
+	case "NOUUIDMATCH" {
+	    logger ("crit", "The serial number of this ISO coul not be found in the database.");
+	}
+	case "NOMORERUNS" {
+	    logger ("crit", "This ISO has been used the maximum number of times.");
+	}
+	case "EXPIREDISO" {
+	    logger ("crit", "This ISO is too old and has expired.");
+	}
+	case "BADCHALLENGE" {
+	    logger ("crit", "The authentication challenge was reject by the authentication manager.");
+	}
+	case "FAILAUTH" {
+	    logger ("crit", "The ISO has encountered a general authentication failure.");
+	}
+	case "TIMEOUT" {
+	    logger ("crit", "The authentication manager timed out.");
+	}
+	else {
+	    logger ("warn", "ISO authenticated.");
+	    return $response;
+	}
+    }
 }
 
+# mount the ENCFS directory. 
+sub mountEncfs {
+    my $password = shift @_;
+    my $enc_dir = "/var/local/.testrig";
+    my $target_dir = "/opt";
+    my %sig;
+    #we need to make sure the directories exist or encfs might hang waiting
+    #for user input
+    if ((! -e $enc_dir) || (! -e $target_dir)) {
+	logger ("crit", "The necessary directories to mount the file system do not exist.");
+    }
+    my $command = "/usr/bin/encfs --extpass=\"echo \'$password\'\" /var/local/.testrig /opt";
+    # the encfs command may hang if something goes wacky. Give it a max of 5 seconds
+    eval {
+	local $sig{ALRM} = sub {die "Timeout\n"};
+	alarm 5;
+	runSystem($command);
+	alarm 0;
+    };
+    # if $@ exists it means that the command timed out
+    if ($@) {
+	logger ("crit", "There was a problem mounting the file system. The command timed out.");
+    }
+}
+
+#----------Test Routines------------#
+
+#get system information
+#interfaces, etc.
+sub getHostData {
+    my $command = "/opt/sbin/ifconfig -a";
+    my $output = runSystem($command, 1);
+    storeOutput ($output, "ifconfig");
+    $command = "/opt/bin/lshw -sanitize";
+    $output = runSystem($command, 1);
+    storeOutput ($output, "hardware");
+}
+
+# we need to make sure that the client can reach the target
+# server. This is not guaranteed so fallbacks are required. 
+# I think it would be safest to try to connect to a known
+# server running on the target. If it connects then we have access.
+# if not then try a different target. The default port for BWCTL is TCP:4823
+sub confirmPath {
+    my $target = shift @_;
+    my $sock = new IO::Socket::INET (
+	PeerAddr => $target,
+	PeerPort => '4823',
+	Proto => 'tcp',
+	Timeout => '5');
+    if (! $sock) {
+	logger ("warn", "Could not connect to $target.");
+	return -1;
+    } else {
+	close ($sock);
+	return $target;
+    }
+}    
+
+# This determins the tests to run and calls the appropriate
+# routines
+sub runTests {
+    use Switch;
+    # config->{user}->{tests} holds a CSV of the
+    # various tests we've been asked to run.
+    # so we turn that into an array and step through it
+    # in a switch statement
+    # as a note all of the necessary target information is in the
+    # config struct.
+    my $test;
+    my @tests = split (/,/, $config->{user}->{tests});
+    foreach $test (@tests) {
+	switch($test) {
+	    case "Traceroute" {
+		testTR();
+	    }
+	    case "Iperf" {
+		testIperf();
+	    }
+	    case "Ping" {
+		testPing();
+	    }
+	    case "Owping" {
+		testOwamp();
+	    }
+	    case "Tcpdump" {
+		testTcpdump();
+	    }
+	    case "UDP" {
+		testUDP();
+	    }
+	}
+    }
+}
+
+sub testTR {
+    # we are actually going to run a numebr of different network
+    # mapping tools here
+    my $target = $config->{user}->{target};
+    logger ("warn", "Running traceroute to $target");
+    my $command = "bwtraceroute -c $target";
+    my $output = ($command, 1);
+    storeOutput($output, "traceroute");
+    logger ("warn", "RUnning tracepath to $target");
+    $command = "bwtraceroute -T tracepath -c $target";
+    $output = ($command, 1);
+    storeOutput($output, "tracepath");
+}
+
+sub testIperf {
+    my $target = $config->{user}->{target};
+    my $command = "bwctl -c $target -t 30 -f m -i 1";
+    logger ("warn", "Running 30 second Iperf test to $target");
+    my $output = runSystem($command, 1);
+    storeOutput($output, "iperf")
+}
+
+sub testPing {
+    my $target = $config->{user}->{target};
+    my $command = "bwping -N 20 -c $target";
+    my $output = runSystem ($command, 1);
+    storeOutput($output, "ping");
+}
+
+sub testOwamp {
+    my $target = $config->{user}->{target};
+    my $command = "bwping -N 20 -T owamp -c $target";
+    my $output = runSystem($command, 1);
+    storeOutput($output, "owamp");
+}
+
+sub testTcpdump {
+    my $target = $config->{user}->{target};
+    my $iface = &getDefault();
+    my $uuid = $config->{user}->{uuid};
+
+    #set up tcpdump to capture the data
+    logger ("warn", "Starting 30 second tcpdump capture");
+    my $command = "/opt/sbin/tcpdump -i $iface -s 100 -w - host $target 2>1 | /bin/gzip > /tmp/results/$uuid-tcpdump.gz &";
+    my $output = runSystem($command, 1); 
+
+    #make sure that the tcpdump worked
+    if (grep /no\ suitable/, $output) {
+	logger ("crit", "tcpdump command failed. Possible sudo falure. Please contact your NOC");
+    }
+    sleep 2;
+
+    # discard the outut from this iperf. we do not care about it.
+    $command = "bwctl -c $target -t 30";
+    runSystem($command);
+
+    #shutdown tcpdump
+    $command = "/usr/bin/sudo killall tcpdump";
+    runSystem ($command);
+}
+
+sub testUDP {
+    my $target = $config->{user}->{target};
+    my $command; 
+    my $output;
+    my @speeds = ("100", "500", "1000");
+    foreach my $speed (@speeds) {
+	logger ("warn", "Running UDP test to $target at $speed Mbps");
+	$command = "bwctl -c $target -i1 -u -b ". $speed . "M";
+	$output = runsystem($command, 1);
+	storeOutput ($output, "UDP-$speed");
+    }
+}
+
+
+
+#-------------MAIN------------------#
 &notice;
+&readConfig;
+&validateConfig;
+my $password = trmanagerAuth();
+mountEncfs($password);
