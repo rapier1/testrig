bac3c4ddee24a7ba37cfb2a4d3883e1f43031aa8
Nate Robinson <nate@psc.edu>
2017-01-13 12:50:12 -0500

    closer to pslocate integration. Touched up form and began process of inspecting ajax responses

--- webGui/main.php
+++ webGui/main.php
@@ -52,7 +52,7 @@
 				  <span class="icon-bar"></span>
 				  <span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Testrig 2.0</a>
+				<a class="navbar-brand" href="<?php ?>">Testrig 2.0</a>
 			</div>
 			<div id="navbar" class="collapse navbar-collapse">
 				  <ul class="nav navbar-nav">
