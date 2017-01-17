d95e1cb338795a94f8aa840e7b8e17ab1ca5c229
blearn <blearn@psc.edu>
2017-01-17 10:16:48 -0500

    Added content to splash page

--- webGui/index.php
+++ webGui/index.php
@@ -74,10 +74,35 @@
 
 
 
-<div id="container-main" class="main-panels">
-<h4 class="text-center">
-Welcome to TestRig 2.0 - a software as a service offering provided by the Pittsburgh Supercomputing Center made possible by a grant from the National Science Foundation. TestRig 2.0 is designed to be a straightforward way to collect valuable network diagnostic information using dynamically generated bootable live ISOs.
-</h4>
+<div id="container-main" class="container">
+  <div class="main-panels">
+  <div class="row">
+    <h4 class="text-center">
+    Welcome to TestRig 2.0 - a software as a service offering provided by the Pittsburgh Supercomputing Center made possible by a grant from the National Science Foundation. TestRig 2.0 is designed to be a straightforward way to collect valuable network diagnostic information using dynamically generated bootable live ISOs.
+    </h4>
+  </div><!-- end row -->
+
+  <div class="row">
+    <div class="col-md-6">
+      <h4 class="text-center">Why TestRig 2.0?<br><br>
+
+Providing support to end users experiencing network performance issues is often a time consuming and involved process. From the initial problem report to resolution it can take days, if not weeks, to gather the necessary diagnostic information, implement suggested fixes, and then re-evaluate performance. Much of this time is spent in the data gathering phase, being that the necessary data is often difficult for end users to collect. 
+
+To address the difficulty of gathering data the Pittsburgh Supercomputing Center's Network Research Team has developed TestRig 2.0; a dynamically generated, customizable, ISO that boots the end user’s machine into a known good environment to run a variety of tests against the perfSONAR network measurement infrastructure. These tests are run automatically and require no special knowledge on the part of the user. The results, which include iperf, tcpdump, traceroute, Web10G data, host information, and more are automatically packaged and returned to the network engineer. Valuable network diagnostic data, which used to require days to gather, can now be had within an hour.
+      </h4>
+    </div>
+    <div class="col-md-6">
+      <h4 class="text-center">How does TestRig 2.0 work?<br><br>
+
+Once your organization signs up for the TestRig 2.0 service you’ll be able to request dynamically generated ISOs. Each ISO is specifically tailored for the user you are providing support for. After entering basic information such as the user’s name, contact information, trouble ticket number, and the name of the host that you want to test you’ll be presented with a set of tests to run against the closest available perfSONAR node. The custom ISO will then be dynamically generated and a download link sent to the end user. Once the user creates bootable media with the ISO, the user will then boot their machine directly into the TestRig 2.0 environment. After agreeing to run the tests, a number of tests will be automatically run and the results stored locally. These results will then be packaged and transferred back to your organization for analysis by network engineers. 
+
+<br><br>Who is TestRig 2.0 for?<br><br>
+
+TestRig 2.0 is aimed at any size institution that needs to provide support to end users that may have difficulties running a full range of network diagnostics on their hosts. Currently the service is provided free for members of the research and education community.
+      </h4>
+    </div>
+  </div><!-- end row -->
+  </div>
 
 
 </div> <!-- END MAIN container -->
